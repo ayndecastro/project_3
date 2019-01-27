c87235cb883c807eda6a42f8b41add4e33115bc1
ayn de castro <vincentayndecastro@gmail.com>
2019-01-27 16:10:15 -0600

    callback issue

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -3,5 +3,5 @@ export const AUTH_CONFIG = {
   clientId: '2yNvl22zVWg6ldtqW03u8FcLSzPoX4TT',
   callbackUrlHeroku: 'https://project3trip.herokuapp.com/callback',
   callbackUrl: 'http://localhost:3000/callback',
-  apiUrl: 'https://safetravels2.com/api/dev'
+  apiUrl: 'https://twilight-flower-8646.auth0.com/api/v2/'
 }
