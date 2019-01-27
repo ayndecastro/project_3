a42b7d45cd4ac306c4bab32f38db55891353ad8b
ayn de castro <vincentayndecastro@gmail.com>
2019-01-27 14:43:10 -0600

    fixed heroku deployment issue

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,6 +1,7 @@
 export const AUTH_CONFIG = {
   domain: 'safe-travels.auth0.com',
   clientId: 'Hb96QtUNLzldu0_BoJgjUOxfOz8t9Rz1',
+  callbackUrlHeroku: 'https://project3trip.herokuapp.com/callback',
   callbackUrl: 'http://localhost:3000/callback',
   apiUrl: 'https://safetravels2.com/api/dev'
 }
