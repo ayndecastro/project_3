494a24fcb657870c88e09b58b520135ba7daf78b
ayn de castro <vincentayndecastro@gmail.com>
2019-01-27 14:49:58 -0600

    callback issue

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,7 +1,7 @@
 export const AUTH_CONFIG = {
   domain: 'safe-travels.auth0.com',
-  clientId: 'Hb96QtUNLzldu0_BoJgjUOxfOz8t9Rz1',
-  callbackUrlHeroku: 'https://project3trip.herokuapp.com/callback',
+  clientId: '2yNvl22zVWg6ldtqW03u8FcLSzPoX4TT',
+  callbackUrlHeroku: 'https://project3trip.herokuapp.com/',
   callbackUrl: 'http://localhost:3000/callback',
   apiUrl: 'https://safetravels2.com/api/dev'
 }
