97cf4aca4c6a9d92500b9000f441373998e18203
ayn de castro <vincentayndecastro@gmail.com>
2019-01-29 16:05:04 -0600

    pulled from van

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'nameless-credit-5551.auth0.com',
   clientId: 'ip1TxG1EA889A-P4Vdmr2kFAco8rVRPi',
-  callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: 'https://project3trip.herokuapp.com/callback',
   apiUrl: 'https://nameless-credit-5551.auth0.com/api/v2/'
 }
