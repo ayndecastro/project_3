1bb1d5607c1d0ee7e7289d01c0c6ecd97f7c2cd5
ayn de castro <vincentayndecastro@gmail.com>
2019-01-29 03:13:19 -0600

    auth-lock

--- server.js
+++ server.js
@@ -52,8 +52,8 @@ require("./routes/default")(app);
 app.use('/api', user)
 
 // If no API routes are hit, send the React app
-app.get('/callback#access_token=Whz4gJheI8VykQBCYmhzwK26l_OkpDDu&id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJnaXZlbl9uYW1lIjoiQXluIiwiZmFtaWx5X25hbWUiOiJEZWNhc3RybyIsIm5pY2tuYW1lIjoiYXluZGVjYXN0cm8iLCJuYW1lIjoiQXluIERlY2FzdHJvIiwicGljdHVyZSI6Imh0dHBzOi8vbGg0Lmdvb2dsZXVzZXJjb250ZW50LmNvbS8tajBYNWlfUXpFc3cvQUFBQUFBQUFBQUkvQUFBQUFBQUFBQUEvQUNldm9RTWxDNmhOM2RxNl9HVnJCSUpXLVBJa2hONVJtQS9tby9waG90by5qcGciLCJnZW5kZXIiOiJtYWxlIiwibG9jYWxlIjoiZW4iLCJ1cGRhdGVkX2F0IjoiMjAxOS0wMS0yOVQwOTowNDo0My43NTZaIiwiaXNzIjoiaHR0cHM6Ly9zYWZlLXRyYXZlbHMuYXV0aDAuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA4OTI2NDUyODc1MjM5MDU1ODQyIiwiYXVkIjoiMnlOdmwyMnpWV2c2bGR0cVcwM3U4RmNMU3pQb1g0VFQiLCJpYXQiOjE1NDg3NTI2ODMsImV4cCI6MTU0ODc4ODY4MywiYXRfaGFzaCI6Im1UN3RXbW1kTTFEa3hBME5SeDlCVkEiLCJub25jZSI6InFyRzh0aU5CT3pffnZJUVF3S0pxZHBzZzZMNHpFUVFiIn0.iYol0X1TGCFEz3NtcJYOTHH7-fuwrNGis70Z2GoQFfQ&expires_in=86400&token_type=Bearer&state=P.3aKEz3Sf0z_u5S9O5bewiF3rLCdUx.', function(req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
  });
 
 
