20cf3b54dbbbf1aaa1cc83c73ebfb6c9077fb458
ayn de castro <vincentayndecastro@gmail.com>
2019-01-29 03:25:11 -0600

    auth-lock

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
-  domain: process.env.REACT_APP_AUTH_DOMAIN_ADDRESS,
-  clientId: process.env.REACT_APP_AUTH_CLIENT_ID,
-  callbackUrl: process.env.REACT_APP_AUTH_CALLBACK_URL,
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  callbackUrl: process.env.AUTH0_CALLBACK_URL,
   apiUrl: process.env.REACT_APP_AUTH_API_URI
 }
